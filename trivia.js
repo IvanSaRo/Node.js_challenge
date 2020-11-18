ccccf7ed7e51f98aebbbd457b758c74955fa277e
Iván Sánchez <ivansancrodr@gmail.com>
2020-11-18 11:45:50 +0100

    :rocket: [Add] Axios added to deal with GET petitions, first GET questions ok

--- trivia.js
+++ trivia.js
@@ -1,15 +1,34 @@
 "use strict";
 
+// Adding minimist to manage command line inputs
 const argv = require("minimist")(process.argv.slice(2));
+// Axios to manage GET petition to triviaAPI
+const axios = require('axios');
+
+
 
 const config = require("./config.js");
 
 const url = config.triviaApiUrl;
 
-
+const getTrivialQuestions = async () => {
+    try {
+      return await axios.get(url);
+    } catch (error) {
+      console.log(`getTrivialQuestions Failed. Error: ${error}`)
+    }
+  }
 
 if (argv.o) {
     console.log(
-      "Write node trivia -p to play, write node trivia -s to see the scores"
+      "Write node trivia -p to play, write node trivia -s to see scores"
     );
   }
+
+  if(argv.p) {
+    const showQuestions = async () => {
+      const response = await getTrivialQuestions();
+      console.log(response.data.results); 
+    }
+    showQuestions();
+}
