a5dfacbf15ddc41fdd3e756ba94129efe4535be4
Iván Sánchez <ivansancrodr@gmail.com>
2020-11-18 16:38:21 +0100

    :hammer: [Fix] Changing the approach to the challenge using readline instead of minimist, we are going to ask first how many questions does the user wants

--- trivia.js
+++ trivia.js
@@ -1,34 +1,38 @@
 "use strict";
 
-// Adding minimist to manage command line inputs
-const argv = require("minimist")(process.argv.slice(2));
 // Axios to manage GET petition to triviaAPI
-const axios = require('axios');
+const axios = require("axios");
+
+// Readline is the perfect tool for this task, I can read inputs from the powershell without reload the script and lose all the info
+let readline = require("readline");
+
+let rl = readline.createInterface(process.stdin, process.stdout);
+
+
+// We check how many answers does the user wants and run a validation over the input
+rl.question("How many questions do you want?(max 20) ", (answer) => {
+  if (isNaN(answer) || answer > 20 || answer <= 0) {
+    rl.write(
+      "Wrong answer, you have to enter a number between 1 and 20, please start again"
+    );
+    process.exit();
+  }
+});
 
 
 
 const config = require("./config.js");
 
-const url = config.triviaApiUrl;
+const url = "https://opentdb.com/api.php?amount=10";
 
-const getTrivialQuestions = async () => {
-    try {
-      return await axios.get(url);
-    } catch (error) {
-      console.log(`getTrivialQuestions Failed. Error: ${error}`)
-    }
+const getTriviaQuestions = async () => {
+  try {
+    response = await axios.get(url);
+    questionsAnswers = response.data.results;
+    return questionsAnswers;
+  } catch (error) {
+    console.log(`getTrivialQuestions Failed. Error: ${error}`);
   }
+};
 
-if (argv.o) {
-    console.log(
-      "Write node trivia -p to play, write node trivia -s to see scores"
-    );
-  }
-
-  if(argv.p) {
-    const showQuestions = async () => {
-      const response = await getTrivialQuestions();
-      console.log(response.data.results); 
-    }
-    showQuestions();
-}
+// getTriviaQuestions();
