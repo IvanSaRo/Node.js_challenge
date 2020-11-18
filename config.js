413c1190fd4d341897764342304c4895ccdb1971
Iván Sánchez <ivansancrodr@gmail.com>
2020-11-18 17:26:32 +0100

    :rocket: [Add] Number of questions delimited by the election of the user, prepare to manage the object served by triviaApi

--- config.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = {
-    triviaApiUrl: 'https://opentdb.com/api.php?amount=10',
-}
--- trivia.js
+++ trivia.js
@@ -1,38 +1,55 @@
 "use strict";
-
 // Axios to manage GET petition to triviaAPI
 const axios = require("axios");
 
 // Readline is the perfect tool for this task, I can read inputs from the powershell without reload the script and lose all the info
 let readline = require("readline");
-
 let rl = readline.createInterface(process.stdin, process.stdout);
 
+let response, questionsAnswers;
 
-// We check how many answers does the user wants and run a validation over the input
-rl.question("How many questions do you want?(max 20) ", (answer) => {
-  if (isNaN(answer) || answer > 20 || answer <= 0) {
-    rl.write(
-      "Wrong answer, you have to enter a number between 1 and 20, please start again"
-    );
-    process.exit();
+rl.question(
+  "Hi, welcome to the Powershell trivia, if you want to play enter p, if you want to see the scores enter s, enter something else to exit\n",
+  (answer) => {
+    // The user has entered p to play
+    if (answer === "p") {
+      // We check how many answers does the user wants and run a validation over the input
+      rl.question("How many questions do you want?(max 20)\n", (answer) => {
+        // We validate the input to prevent any failure geting the info from triviaApi
+        if (isNaN(answer) || answer > 20 || answer <= 0) {
+          rl.write(
+            "Wrong answer, you have to enter a number between 1 and 20, please start again"
+          );
+          process.exit();
+        }
+        //  If the validation goes ok we pass the number of questions through parameter
+        getTriviaQuestions(answer);
+      });
+    } else if (answer === "s") {
+      console.log("scores");
+    } else {
+      process.exit();
+    }
   }
-});
+);
 
-
-
-const config = require("./config.js");
-
-const url = "https://opentdb.com/api.php?amount=10";
-
-const getTriviaQuestions = async () => {
+// We get the number of answers by parameter 
+const getTriviaQuestions = async (numberOfQuestions) => {
   try {
+    const url = `https://opentdb.com/api.php?amount=${numberOfQuestions}`;
+
     response = await axios.get(url);
     questionsAnswers = response.data.results;
-    return questionsAnswers;
+    manageAndPresentQuestions(questionsAnswers);
+    
   } catch (error) {
     console.log(`getTrivialQuestions Failed. Error: ${error}`);
   }
 };
 
-// getTriviaQuestions();
+
+const manageAndPresentQuestions = (questionsAnswers) => {
+
+    console.log(questionsAnswers);
+
+}
