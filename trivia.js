aceca56953af7cc9663dece61ecb0ef501b1f5a5
Iván Sánchez <ivansancrodr@gmail.com>
2020-11-19 20:21:42 +0100

    :rocket: [Add] Score validation ok, ready to save scores

--- trivia.js
+++ trivia.js
@@ -1,20 +1,20 @@
 "use strict";
 // Axios to manage GET petition to triviaAPI
 const axios = require("axios");
+const { type } = require("os");
 
 // Readline is the perfect tool for this task, I can read inputs from the powershell without reload the script and lose all the info
 let readline = require("readline");
 let rl = readline.createInterface(process.stdin, process.stdout);
 
 let response, questionsAnswers;
-let questions = [];
 let answers = [];
 let correctAnswer = [];
-let incorrectAnswers = [];
 let randomAnswers = [];
 let questionPlusAnswers = [];
-let selectedAnswers = [];
 
+
+// Here is where we start the cycle
 rl.question(
   "Hi, welcome to the Powershell trivia, if you want to play enter p, if you want to see the scores enter s, enter something else to exit\n",
   (answer) => {
@@ -57,14 +57,12 @@ const getTriviaQuestions = async (numberOfQuestions) => {
 // Here we are going to deconstruct the object and prepare it to show it to the user
 const manageAndPresentQuestions = (questionsAnswers) => {
   questionsAnswers.forEach((element) => {
-    correctAnswer.push(element.correct_answer);
-
     answers = [...element.incorrect_answers, element.correct_answer];
 
     randomAnswers = answers.sort(function () {
       return Math.random() - 0.5;
     });
-
+// After getting the answers and randomize it we create the object with the question and the answers
     questionPlusAnswers.push({
       question: element.question,
       A: randomAnswers[0],
@@ -72,6 +70,21 @@ const manageAndPresentQuestions = (questionsAnswers) => {
       C: randomAnswers[2],
       D: randomAnswers[3],
     });
+
+    // with this function we send the correct answer in a way that can be compared with the user selection, we send the key, not the value
+    function getKeyByValue(object, value) {
+      object.map((object) => {
+        const correctKey = Object.keys(object).find(
+          (key) => object[key] === value
+        );
+        correctAnswer.push(correctKey);
+        correctAnswer = correctAnswer.filter(function (value) {
+          return value != undefined;
+        });
+      });
+    }
+
+    getKeyByValue(questionPlusAnswers, element.correct_answer);
 
     // Now we have the question and the answers and the correct answer to check if the user has answered correctly, we are ready to send this information and show it to the user
   });
@@ -81,13 +94,12 @@ const manageAndPresentQuestions = (questionsAnswers) => {
 
 // this function receives the questions and shows it to the user, also recives te correct answers to validate user´s answers
 const showCuestions = (questions, correctAnswer) => {
-  let questionsToValidate = [...questions];
+  
 
   let userAnswers = [];
 
   function ask(i) {
     let processedQuestion = giveFormatToQuestionObject(questions[i]);
-
     process.stdout.write(processedQuestion);
   }
 
@@ -96,15 +108,16 @@ const showCuestions = (questions, correctAnswer) => {
 
     data = data.toString().toUpperCase();
 
-    // console.log(" aqui ", typeof data, data);
-
     if (data === "A" || data === "B" || data === "C" || data === "D") {
       userAnswers.push(data);
 
       if (userAnswers.length < questions.length) {
         ask(userAnswers.length);
       } else {
-        console.log(userAnswers);
+        let score = calculateScore(userAnswers, correctAnswer);
+        process.stdout.write(
+            "\nYour score is " + score +"% correct questions\n"
+          );
         process.exit();
       }
     } else {
@@ -124,13 +137,13 @@ const giveFormatToQuestionObject = (questionObject) => {
   let questionString = JSON.stringify(questionObject);
 
   questionString = questionString.replace(
-    /[-!$%^*()_+|~=`{}\[\]";'<>?,.\/]/g,
+    /[-!$%^*()_+|~=`{}\[\]";'<>?,.\/&]/g,
     " "
   );
   questionString = questionString.slice(2);
   questionString =
     questionString.charAt(0).toUpperCase() + questionString.slice(1);
-  //   questionString = questionString.map()
+
   let lineBreak = "\n";
   questionString = questionString.concat(lineBreak);
   questionString = lineBreak + questionString;
@@ -138,3 +151,16 @@ const giveFormatToQuestionObject = (questionObject) => {
   return questionString;
 };
 
+// here we are going to calculate the score
+const calculateScore = (userAnswers, correctAnswer) => {
+  let iterations = userAnswers.length;
+  let points = 0;
+
+  for (let i = 0; i < iterations; i++) {
+    if (userAnswers[i] === correctAnswer[i]) {
+      points++;
+    }
+  }
+// now we are returning the score in an appropriate format to show it and to store it
+  return Math.trunc((points / userAnswers.length) * 100);
+};
